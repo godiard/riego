b2b8cb34092af53b99e46cfe8f4dcfe6b30cf1a7
Gonzalo Odiard <godiard@gmail.com>
2021-03-14 00:06:14 -0300

    Make jobs.py monitor changes on config.json

--- jobs.py
+++ jobs.py
@@ -3,7 +3,12 @@ import json
 import schedule
 import time
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
 CONFIG_FILE_NAME = 'config.json'
+
+config = None
 
 
 def read_config():
@@ -27,6 +32,7 @@ def stop_irrigation(port, power_port):
 
 
 def init_scheduled_tasks(config):
+    print('config: %s' % config)
     power_port = config['power']['port']
     for zone in config['zones']:
         # zone['start'] have the format '22:30'
@@ -39,9 +45,32 @@ def init_scheduled_tasks(config):
             stop_irrigation, zone['port'], power_port)
 
 
+def update_config(event):
+    global config
+    print('CONFIG FILE UPDATED')
+    updated_config = read_config()
+    if updated_config == config:
+        print('FILE NOT CHANGED')
+    else:
+        config = updated_config
+        init_scheduled_tasks(config)
+
+
 config = read_config()
-print(config)
 init_scheduled_tasks(config)
+
+event_handler = FileSystemEventHandler()
+event_handler.on_modified = update_config
+
+observer = Observer()
+observer.schedule(event_handler, CONFIG_FILE_NAME, recursive=False)
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
 
 while True:
     schedule.run_pending()
